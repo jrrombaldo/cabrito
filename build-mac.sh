e952f4b65db2f8f14442bbfc42ebbf7bba45276f
Carlos Rombaldo <carlos.rombaldo@10xbanking.com>
2019-03-08 15:40:28 +0000

    addiing releases

--- build-mac.sh
+++ build-mac.sh
@@ -1,6 +1,22 @@
+#!/bin/bash
+
+# Setup script error handling see https://disconnected.systems/blog/another-bash-strict-mode for details
+set -xuo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+IFS=$'\n\t'
+
+NAME="Cabrito"
+VERSION="0.1"
+
+
+npm install 
+
 PATH=$(npm bin):$PATH
 
+
 electron-packager . \
+--appname=$NAME \
+--build-version$VERSION \
 --overwrite \
 --platform=darwin \
 --arch=x64 \
@@ -8,4 +24,8 @@ electron-packager . \
 --prune=true \
 --out=release-builds \
 --darwin-dark-mode-support 
+# --out="./"
 # cp -r ./release-builds/*/*.app ./
+
+REL_FILE=$(find . -type d -name "$NAME.app")
+echo "file created is $REL_FILE"
